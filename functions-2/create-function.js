e19cd1ab360b52e0be2a3a9d71946f74e696e66d
Omar AlBarakeh <omaralbarakeh2@gmail.com>
2024-10-31 01:11:35 +0200

    dude and it animations  uploaded

---
+++ functions-2/create-function.js
@@ -0,0 +1,4 @@
+    player = this.physics.add.sprite(50, 50, "dude");
+    this.anims.create({ key: "turn", frames: [{ key: "dude", frame: 4 }], frameRate: 20 });
+    this.anims.create({ key: "left", frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }), frameRate: 10, repeat: -1 });
+    this.anims.create({ key: "right", frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }), frameRate: 10, repeat: -1 });
