361c775c07f4fcc718a9b1c049ef1f6ece54959e
Omar AlBarakeh <omaralbarakeh2@gmail.com>
2024-10-31 01:12:38 +0200

    colide between walls and player added

--- functions-2/create-function.js
+++ functions-2/create-function.js
@@ -2,3 +2,6 @@
     this.anims.create({ key: "turn", frames: [{ key: "dude", frame: 4 }], frameRate: 20 });
     this.anims.create({ key: "left", frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }), frameRate: 10, repeat: -1 });
     this.anims.create({ key: "right", frames: this.anims.generateFrameNumbers("dude", { start: 5, end: 8 }), frameRate: 10, repeat: -1 });
+
+
+    this.physics.add.collider(player, this.walls);
